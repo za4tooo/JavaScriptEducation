dfb3765af93dfd49c28f6f1ca80032a0acdbc1ea
DESKTOP-5V2S9D4\Slava <vyacheslav.saulin@indusoft.ru>
2023-03-24 16:56:47 +0700

    ex.39

---
+++ projectPractice39/script.js
@@ -0,0 +1,40 @@
+const btn = document.querySelector('.btn');
+
+let timeId,
+i = 0;
+
+
+
+function myAnimation(){
+    const elem = document.querySelector('.box');
+    let pos = 0;
+
+    const id = setInterval(frame, 10);
+    function frame(){
+        if(pos==300){
+            clearInterval(id);
+        }
+        else{
+            pos++;
+            elem.style.top = pos + "px";
+            elem.style.left = pos + 'px';
+        }
+    }
+}
+
+
+function logger (){
+    if(i===3)
+    {
+        clearInterval(timeId);
+    }
+    console.log('text');
+    i++;
+}
+btn.addEventListener('click', myAnimation);
+
+
+// let id = setTimeout(function log(){
+//     console.log('Hello');
+//     id = setTimeout(log, 500);
+// }, 500);
